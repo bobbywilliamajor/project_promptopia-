bd14afab3e049373c8ac42dccd0a2ae59b45430a
bobbywilliamajor <bobbywilliamajor@gmail.com>
2024-01-27 10:32:49 +0200

    updated files

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,7 +21,9 @@ const Feed = () => {
   const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([])
 
-  const handleSearchChange = (e) => {}
+   // Search states
+   const [searchTimeout, setSearchTimeout] = useState(null)
+   const [searchedResults, setSearchedResults] = useState([])
 
   useEffect(()=> {
     const fetchPosts = async () => {
@@ -32,24 +34,60 @@ const Feed = () => {
 
     fetchPosts()
   }, [])
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i") // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
   return (
-    <section className="feed">
-      <form className="relative w-full flex-center">
-        <input 
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
           type='text'
-          placeholder="Search for a tag or a username"
+          placeholder='Search for a tag or a username'
           value={searchText}
           onChange={handleSearchChange}
           required
-          className="search_input peer"
+          className='search_input peer'
         />
       </form>
 
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() =>{}}/>
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default Feed
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,12 +4,21 @@ import { useState } from "react"
 import Image from "next/image"
 import { useSession } from "next-auth/react"
 import { usePathname, useRouter } from "next/navigation"
+import Link from "next/link"
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const [copied, setCopied] = useState("")
   const {data: session} = useSession()
   const pathName = usePathname()
   const router = useRouter()
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
 
   const handleCopy = () => {
     setCopied(post.prompt)
@@ -20,7 +29,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3
-          cursor-point">
+          cursor-pointer" onClick={handleProfileClick}>
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -33,7 +42,8 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
                {post.creator.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-                {post.creator.email}</p>
+                 {post.creator.email}
+              </p>
           </div>
         </div>
 
@@ -52,9 +62,9 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       <p className="my-4 font-satoshi text-sm text-gray-700">
         {post.prompt}
       </p>
-      <p className="font-inter text-sm blue_gradient cursor_pointer"
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={()=> handleTagClick && handleTagClick(post.tag)}>
-        #{post.tag}
+        {post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' 
